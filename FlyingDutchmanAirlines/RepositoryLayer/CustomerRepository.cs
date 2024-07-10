efb39653235faa982951121da79cc2fee92640a9
Raymond McDonald <contact@wreckingballpublishing.com>
2024-07-10 13:50:05 -0500

    Create empty new CustomerRepository class to build a skeleton on for test-driven development

---
+++ FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
@@ -0,0 +1,10 @@
+namespace FlyingDutchmanAirlines.RepositoryLayer
+{
+    public class CustomerRepository
+    {
+        public void CreateCustomer(string name)
+        {
+            
+        }
+    }
+}
