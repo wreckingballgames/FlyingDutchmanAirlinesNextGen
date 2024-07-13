102300bed0ec4d10543db8c760ab21988838b9dd
Raymond McDonald <contact@wreckingballpublishing.com>
2024-07-13 14:09:55 -0500

    Add new equality comparison functionality to Customer class

--- FlyingDutchmanAirlines/DatabaseLayer/Models/Customer.cs
+++ FlyingDutchmanAirlines/DatabaseLayer/Models/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Collections.Generic;
 
 namespace FlyingDutchmanAirlines.DatabaseLayer.Models;
@@ -16,4 +17,28 @@ public sealed class Customer
         Name = name;
     }
 
+        public static bool operator == (Customer x, Customer y)
+    {
+        CustomerEqualityComparer comparer = new();
+        return comparer.Equals(x, y);
+    }
+
+    public static bool operator != (Customer x, Customer y)
+    {
+        return !(x == y);
+    }
+}
+
+internal class CustomerEqualityComparer : EqualityComparer<Customer>
+{
+    public override int GetHashCode(Customer obj)
+    {
+        int randomNumber = RandomNumberGenerator.GetInt32(int.MaxValue / 2);
+        return (obj.CustomerId + obj.Name.Length + randomNumber).GetHashCode();
+    }
+
+    public override bool Equals(Customer x, Customer y)
+    {
+        return x.CustomerId == y.CustomerId && x.Name == y.Name;
+    }
 }
