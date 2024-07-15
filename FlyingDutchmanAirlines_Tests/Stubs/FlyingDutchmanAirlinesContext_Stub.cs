3cfdb44426446d6d5e79bdbf0292dd501f7fd0df
Raymond McDonald <contact@wreckingballpublishing.com>
2024-07-15 15:30:14 -0500

    Add empty context stub to test project

---
+++ FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
@@ -0,0 +1,8 @@
+using FlyingDutchmanAirlines.DatabaseLayer.Models;
+
+namespace FlyingDutchmanAirlines_Tests;
+
+public class FlyingDutchmanAirlinesContext_Stub : FlyingDutchmanAirlinesContext
+{
+
+}
